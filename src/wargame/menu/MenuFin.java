8d98d565bd48a08ab6c3f059dfdd0c71090218a8
diall <diall@161.3.48.19>
2019-12-05 16:55:34 +0100

    feat : IA et debut Menufin

---
+++ src/wargame/menu/MenuFin.java
@@ -0,0 +1,12 @@
+package wargame.menu;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import wargame.Wargame;
+
+public class MenuFin extends MenuNouvellePartie {
+	public MenuFin(Wargame jeu, int fin) {
+	super(jeu);
+	}
+}
